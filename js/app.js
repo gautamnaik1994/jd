826ac3035375946a1422af63f418832890f2aa0d
Gautam Naik <gautamnaik1994@gmail.com>
2017-07-02 10:12:21 +0530

    added category listing page

--- js/app.js
+++ js/app.js
@@ -4,6 +4,17 @@ $(window).load(function () {
     controlNav: false
   });
 });
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip(); 
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
 });
+
+// Set Search filter 
+function setFilter() {
+  let elem = document.querySelector('#selectSearchFilter');
+  // switch (elem.options[elem.selectedIndex].value)
+  // {
+  //   case 'sortBy':
+  // }
+  document.querySelector('.sort-btn').classList.toggle('hide');
+  document.querySelector('.search-by').classList.toggle('hide');
+}
