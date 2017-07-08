471aa09cbbea2e7014de7a011b3d427c49bf403a
Gautam Naik <gautamnaik1994@gmail.com>
2017-07-08 17:46:01 +0530

    added merchant profile page

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
-$(window).load(function () {
-  $('.flexslider').flexslider({
-    animation: "slide",
-    controlNav: false
-  });
-});
+
 $(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
